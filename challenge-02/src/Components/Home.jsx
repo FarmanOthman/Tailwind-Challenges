8da5bef5d50ad684b2e97ed10da2b8bdd371d342
FarmanOthman <farmaanothmaan@gmail.com>
2025-02-22 10:05:11 +0300

    Add Nav bar

--- challenge-02/src/Components/Home.jsx
+++ challenge-02/src/Components/Home.jsx
@@ -1,7 +1,12 @@
+import Nav from "./Nav"
+
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Home</h1>
+      <Nav />
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-4xl">Home</h1>
+      </div>
     </div>
   )
 }
--- challenge-02/src/Components/Nav.jsx
+++ challenge-02/src/Components/Nav.jsx
@@ -1,7 +1,16 @@
-
+import { NavLink } from 'react-router-dom'
+import logo from '../assets/images/logo-bookmark.svg'
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className=" flex justify-around flex-row-reverse bg-white p-6">
+        <div className="flex items-center text-black mr-6 justify-between flex-row-reverse">
+          <NavLink to="/" className="nav-link" activeClassName="active">Login</NavLink>
+          <NavLink to="/about" className="nav-link" activeClassName="active">Content</NavLink>
+          <NavLink to="/contact" className="nav-link" activeClassName="active">Pricing</NavLink>
+          <NavLink to="/contact" className="nav-link" activeClassName="active">Future</NavLink>
+        </div>
+        <img src={logo} alt="Logo" className="h-8  max-sm:pl-4" />
+      </nav>
   )
 }
 
