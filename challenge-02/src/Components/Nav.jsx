c1631ca7f05848cac1d9df0278b69d81c04c1776
FarmanOthman <farmaanothmaan@gmail.com>
2025-02-22 11:30:43 +0300

    Implement responsive navigation menu and add Features component

--- challenge-02/src/Components/Nav.jsx
+++ challenge-02/src/Components/Nav.jsx
@@ -1,16 +1,44 @@
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 import logo from '../assets/images/logo-bookmark.svg'
+
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <nav className=" flex justify-around flex-row-reverse bg-white p-6">
-        <div className="flex items-center text-black mr-6 justify-between flex-row-reverse">
+    <nav className="flex justify-between items-center bg-white p-6">
+      <div className="flex items-center">
+        <img src={logo} alt="Logo" className="h-8" />
+      </div>
+      
+      <div className="md:flex hidden items-center space-x-6 text-black">
+        <NavLink to="/" className="nav-link" activeClassName="active">Login</NavLink>
+        <NavLink to="/about" className="nav-link" activeClassName="active">Content</NavLink>
+        <NavLink to="/contact" className="nav-link" activeClassName="active">Pricing</NavLink>
+        <NavLink to="/contact" className="nav-link" activeClassName="active">Future</NavLink>
+      </div>
+
+      <div className="md:hidden">
+        <button onClick={toggleMenu} className="text-black">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+      </div>
+
+      {isOpen && (
+        <div className="md:hidden absolute top-20 left-0 right-0 bg-white p-6 space-y-4 shadow-md">
           <NavLink to="/" className="nav-link" activeClassName="active">Login</NavLink>
           <NavLink to="/about" className="nav-link" activeClassName="active">Content</NavLink>
           <NavLink to="/contact" className="nav-link" activeClassName="active">Pricing</NavLink>
           <NavLink to="/contact" className="nav-link" activeClassName="active">Future</NavLink>
         </div>
-        <img src={logo} alt="Logo" className="h-8  max-sm:pl-4" />
-      </nav>
+      )}
+    </nav>
   )
 }
 
